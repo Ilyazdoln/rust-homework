87f21f04ddcb6ceaaa1a25132f947d759ad3b899
іІлля здольник <vbvnf227gmail.com>
2025-05-25 22:20:59 +0300

    Додав завдання hw03: ASCII конверт

--- src/hackerrank/day02.rs
+++ src/hackerrank/day02.rs
@@ -2,20 +2,25 @@ const W: usize = 20;
 const H: usize = 10;
 
 fn main() {
+    // Зберемо весь малюнок у один String, щоб потім вивести print! один раз
     let mut envelope = String::new();
 
     for row in 0..H {
         for col in 0..W {
+            // Верхній і нижній рядок
             if row == 0 || row == H - 1 {
                 if col == 0 || col == W - 1 {
                     envelope.push('+');
                 } else {
                     envelope.push('-');
                 }
-            } else {
+            }
+            // Бокові рядки з "клапанами"
+            else {
                 if col == 0 || col == W - 1 {
                     envelope.push('|');
                 } else {
+                    // Розрахунок діагоналей
                     let left_diag = col == row;
                     let right_diag = col == W - row - 1;
 
@@ -34,4 +39,3 @@ fn main() {
 
     print!("{}", envelope);
     println!();
-}
